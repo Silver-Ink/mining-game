d2bc063c1cd2a55a49bc0baad84e686508b601b9
Thomas-Mewily <thomas.mewily@gmail.com>
2026-05-29 11:06:29 +0200

    fixed wrong idx

--- tile/shape_sprite.gd
+++ tile/shape_sprite.gd
@@ -3,7 +3,7 @@ class_name ShapeSprite
 
 #@export var tiled_node : Node2D = Node2D.new()
 #@export var tileset : TileSet = null
-@export var tileset_sprite : Sprite2D = null
+@export var tileset_sprite : Texture2D = null
 @export var tiled_sprite : Sprite2D  = null
 @export var global_sprite : Sprite2D = null
 const TILE_SIZE : int = 8
@@ -25,17 +25,18 @@ func update(shape: Shape):
 		#add_child(new_tiled_node)
 		
 	if tileset_sprite:
-		var idx_x = 0
-		var idx_y = 0
 		for tile in shape.tiles():
-			idx_x += 3 - ((shape.tile.contains(tile + Vector2i(-1,0)) as int) + (shape.tile.contains(tile + Vector2i(+1,0)) as int * 2))
-			idx_y += 3 - ((shape.tile.contains(tile + Vector2i(0,-1)) as int) + (shape.tile.contains(tile + Vector2i(0,+1)) as int * 2))
+			var idx_x = 3 - ((shape.tile.contains(tile + Vector2i(-1,0)) as int) + (shape.tile.contains(tile + Vector2i(+1,0)) as int * 2))
+			var idx_y = 3 - ((shape.tile.contains(tile + Vector2i(0,-1)) as int) + (shape.tile.contains(tile + Vector2i(0,+1)) as int * 2))
 			
-			var new_sprite = tileset_sprite.duplicate()
+			var new_sprite = Sprite2D.new() # tileset_sprite.duplicate()
+			new_sprite.texture = tileset_sprite
 			new_sprite.centered = false
 			new_sprite.region_enabled = true
 			new_sprite.region_rect = Rect2i(idx_x * TILE_SIZE, idx_y * TILE_SIZE, TILE_SIZE, TILE_SIZE)
+			#print(new_sprite.region_rect)
 			#print(tile)
+			new_sprite.scale = 4 * TILE_SCALE;
 			new_sprite.position = Vector2(tile.x * TILE_SIZE, tile.y * TILE_SIZE)
 			add_child(new_sprite)
 	
@@ -47,9 +48,8 @@ func update(shape: Shape):
 
 static func tileset(uid : String) -> ShapeSprite:
 	var s = new()
-	s.tileset_sprite = Sprite2D.new()
-	s.tileset_sprite.texture = load(uid) # pretty sure it is not opti / will be duplicated
-	s.tileset_sprite.scale = 4 * TILE_SCALE;
+	s.tileset_sprite = load(uid) # pretty sure it is not opti / will be duplicated
+	#s.tileset_sprite.scale = 4 * TILE_SCALE;
 	return s
 
 
