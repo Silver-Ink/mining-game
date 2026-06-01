613e7f11a7c9c48ebf2155c6668f663b200f513f
Silver-Ink <martin@clarimo.xyz>
2026-06-01 07:53:20 +0200

    Squashed commit of the following:
    commit a087243ad87442e1e2f74b9402eb995342fe41f4
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Sun May 24 16:35:56 2026 +0200
    
        searchable area
    
    commit 433b9a435c4324e927c21489c8d8bebd925be798
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Sun May 24 16:20:53 2026 +0200
    
        _
    
    commit 30dd0d6bea3b047acbee1544ec3bc048c5f46bcb
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Sun May 24 16:20:49 2026 +0200
    
        a bunch of new treasure/objet with interesting/non fully filled rectangle shape
    
    commit df05d71d52b6f1297b2cb085a85ae29625e5c702
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Sun May 24 14:04:04 2026 +0200
    
        skull saber
    
    commit d0ebc3926ba516057999f6651581e5e49f303ec4
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Sat May 23 18:26:19 2026 +0200
    
        skull saber
    
    commit a2a974393f5abf1ddb07ec47b6a04be88a341bb8
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Sat May 23 18:19:18 2026 +0200
    
        some fossil
    
    commit 173e587b682d6b291f440fd1d8f2effcd619bc7d
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Sat May 23 17:54:26 2026 +0200
    
        assets, stone, ladder
    
    commit f69b76e2400334489abe551887400209e118805d
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Thu May 21 16:24:17 2026 +0200
    
        tmp and bone
    
    commit 5e9af1f13b0d048f3f9fc6dd9e46786d0c326c83
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Thu May 21 15:10:05 2026 +0200
    
        wall
    
    commit 529cbcb84fe161587d45a49f28ef5e516ca2cb66
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Thu May 21 15:03:45 2026 +0200
    
        wall
    
    commit 42a439f26d64ad123e6d118bf40692f246beb018
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Thu May 21 14:14:56 2026 +0200
    
        rock
    
    commit 74a373e878c4d65de3cf5f7fc31d1bc5eaffb2dc
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Thu May 21 12:31:48 2026 +0200
    
        sand
    
    commit 2ad0e7eb6f6e9f19bebc6fc65f7d1a5470851a02
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Thu May 21 11:20:33 2026 +0200
    
        some sprite
    
    commit 7beb7f97dee60846393363d783f7f201d75cdc7d
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Mon May 18 10:28:43 2026 +0200
    
        update readme
    
    commit ef292ab7b9b7a166e748e6351b59a03b6f370397
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Mon May 18 10:28:41 2026 +0200
    
        update readme
    
    commit b89ead0e8c26c07bcff649cce7b2c89a25bd1a8b
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Mon May 18 10:28:29 2026 +0200
    
        update readme
    
    commit 26421ca0471ca0f0f21bb2998c459b3498b5f689
    Author: Thomas-Mewily <thomas.mewily@gmail.com>
    Date:   Mon May 18 10:27:27 2026 +0200
    
        update git ignore

--- mining_wall.gd
+++ mining_wall.gd
@@ -1,5 +1,19 @@
 class_name MiningWall
 extends Interractible
 
+@export var variant : MiningWallVariant:
+	set(value):
+		variant = value
+		match value:
+			MiningWallVariant.Ladder:
+				pass
+			MiningWallVariant.Hole:
+				pass
+	
+enum MiningWallVariant{
+	Ladder,
+	Hole
+}
+
 func interract(caller : Character):
 	print("hello")
