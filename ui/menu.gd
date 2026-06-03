c6b8e5f13e1da37ef27212b9ccdeb00ea034d2ad
Silver-Ink <martin@clarimo.xyz>
2026-06-03 15:16:59 +0200

    fix warp zone containing player on spawn

--- ui/menu.gd
+++ ui/menu.gd
@@ -4,6 +4,6 @@ func _ready() -> void:
 	_on_button_button_down()
 	
 func _on_button_button_down() -> void:
-	SceneManager.push_scene(SceneManager.SceneId.LevelA, MiningArea.MiningAreaSceneSettings.new(0))
+	SceneManager.push_scene(SceneManager.SceneId.LevelA, MiningArea.MiningAreaSceneSettings.new(-1))
 	self.queue_free()
 	
